8e2b8819f1302b7400cfd9dc91ea34c69d7786f5
lStewieAl <lStewieAl@users.noreply.github.com>
2018-11-26 15:46:38 +0000

    Instead of not jumping, only jump if NPC flag set

--- main.cpp
+++ main.cpp
@@ -28,20 +28,22 @@ extern "C" {
 
 
 void patchAimingChecks() {
-	// When moving between left/right, front/back
-	//0x492F39 - je 0x492F9B
-	UInt32 isAimingCheckAddr = 0x492F39;
-	SafeWriteBuf(isAimingCheckAddr, "\x90\x90", 2);
+  // Set the NPC flag on the AND, so it always evaluates false for the player
 
-	// Forward/Sideward movement
-	//0x896B91 - je 0x896C17
-	isAimingCheckAddr = 0x896B91;
-	SafeWriteBuf(isAimingCheckAddr, "\x90\x90\x90\x90\x90\x90", 6);
+  // When moving between left/right, front/back
+  //0x492F33 - and edx,00000240
+  UInt32 isAimingCheckAddrAnd = 0x492F33;
+  SafeWriteBuf(isAimingCheckAddrAnd, "\x81\xE2\x40\x02\x00\x00", 6);
 
-	// Diagonal movement
-	//0x8A0B4B - je 0x8A0B92
-	isAimingCheckAddr = 0x8A0B4B;
-	SafeWriteBuf(isAimingCheckAddr, "\x90\x90", 2);
+  // Forward/Sideward movement
+  //0x896B8C - and eax,00000200
+  isAimingCheckAddrAnd = 0x896B8C;
+  SafeWriteBuf(isAimingCheckAddrAnd, "\x25\x40\x02\x00\x00", 5);
+
+  // Diagonal movement
+  //0x8A0B46 - and eax,00000200
+  isAimingCheckAddrAnd = 0x8A0B46;
+  SafeWriteBuf(isAimingCheckAddrAnd, "\x25\x40\x02\x00\x00", 5);
 }
 
 
